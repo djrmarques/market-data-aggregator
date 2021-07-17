67196cb4a882482aff4db8f30c035287a24b3de7
davidmarques <davidmarques856@gmail.com>
2021-07-17 22:24:12 +0100

    Added latest coinmarketcap cli

--- data_aggregator/crypto/coinmarketcap.py
+++ data_aggregator/crypto/coinmarketcap.py
@@ -2,8 +2,8 @@ import logging
 import pandas as pd
 import requests
 import datetime 
-from typing import Union
 import os
+from typing import Union
 from datetime import datetime as dt
 
 logging.basicConfig(level=logging.INFO)
@@ -15,8 +15,8 @@ class CoinmarketcapWrapper:
 
     def __init__(self, api_key: str, s3_bucket: str):
         self.latest = pd.DataFrame() # Filled by get_latest method
-        self.s3_bucket = api_key
-        self.api_key = s3_bucket
+        self.api_key = api_key
+        self.s3_bucket = s3_bucket
 
     def download_and_save_all_tickers(self, slug: str, start_date: Union[dt, None], end_date: Union[dt, None]) -> pd.DataFrame:
         """ Go through the list of all the tickers, downloads the daily price and saves them to S3."""
@@ -36,19 +36,39 @@ class CoinmarketcapWrapper:
         """
         raise NotImplementedError
 
-    def get_latest(self) -> pd.DataFrame:
+    def get_latest(self, local_save=False) -> pd.DataFrame:
         """ This is the only call with the official API """
-        url = self.API_ADDRESS + r'/v1/cryptocurrency/listings/latest'
+        url = r"https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest"
 
         # CMC_PRO_API_KEY
         params = {'CMC_PRO_API_KEY': self.api_key, 'limit': self.LIMIT}
-        self.latest = pd.DataFrame(requests.get(url, params=params).json()['data'])
-        logging.info(f"Downloaded {self.latest.shape[0]} tokens with more than {self.MARKETCAP_LIMIT} Market Cap")
+        try:
+            self.latest = pd.DataFrame(map(self.process_quote, requests.get(url, params=params).json()['data']))
+        except KeyError as e:
+            logging.error(params)
+            raise e
+        except Exception as e:
+            raise e
+        logging.info(f"Downloaded {self.latest.shape[0]} tokens")
 
         # Save output to S3
         current_time = dt.now().strftime(r"%Y%m%d%H%M")
-        s3_save_path =  self.s3_bucket + "/hourly_cmk" + f"/cmk_latest_{current_time}.parquet"
-        self.info(f'Saving file to {s3_save_path}')
-        self.latest.to_parquet(s3_save_path)
+        if not local_save:
+            s3_save_path =  self.s3_bucket + "/hourly_cmk" + f"/cmk_latest_{current_time}.parquet"
+            logging.info(f'Saving file to {s3_save_path}')
+            self.latest.to_parquet(s3_save_path)
+        else:
+            self.latest.to_csv()
         return self.latest 
+
+    @staticmethod
+    def process_quote(input_quote: dict) -> dict:
+        """Processed the CMK api latest quotes """
+        base_cols_to_keep = {k: v for k, v in input_quote.items() if k != 'quote'}
+        currency_quotes_cols = ('price', 'volume_24h', 'market_cap')
+        for currency, quote in input_quote['quote'].items():
+            quotes = {f"{currency}_{k}": v for k, v in quote.items()  if k in currency_quotes_cols}
+            quotes.update({k: v for k, v in quote.items() if k not in currency_quotes_cols})
+
+        return {**base_cols_to_keep, **quotes}
 
